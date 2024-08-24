f15c9be75eac2dd00113d54c7921ab8ea6b384c7
Vincent Sposato <vincent.sposato@gmail.com>
2024-08-24 14:37:55 -0400

    Lesson 1.4.3 take control of the vehicle speed

--- Unit_1-Player Control/Prototype 1/Assets/Scripts/PlayerController.cs
+++ Unit_1-Player Control/Prototype 1/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
     public float speed = 20.0f;
     public float turnSpeed;
     public float horizontalInput;
+    public float forwardInput;
 
     // Start is called before the first frame update
     void Start()
@@ -19,9 +20,10 @@ public class PlayerController : MonoBehaviour
     {
         // Get the user's movements
         horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
 
         // Move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
         transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
