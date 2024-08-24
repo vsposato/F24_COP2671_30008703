bc58545e64e3ebd130343507a76030ffdf7aa56d
Vincent Sposato <vincent.sposato@gmail.com>
2024-08-24 14:32:40 -0400

    Lesson 1.3.5 smooth the camera with lateupdate

--- Unit_1-Player Control/Prototype 1/Assets/Scripts/FollowPlayer.cs
+++ Unit_1-Player Control/Prototype 1/Assets/Scripts/FollowPlayer.cs
@@ -13,8 +13,8 @@ public class FollowPlayer : MonoBehaviour
         
     }
 
-    // Update is called once per frame
-    void Update()
+    // LateUpdate is called once per frame
+    void LateUpdate()
     {
         transform.position = player.transform.position + offset;
     }
