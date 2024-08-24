8fee4bf72590af39fb6ceb947901b92b1f1eccd5
Vincent Sposato <vincent.sposato@gmail.com>
2024-08-24 14:36:35 -0400

    Lesson 1.4.2 base left/right movement on input

--- Unit_1-Player Control/Prototype 1/Assets/Scripts/PlayerController.cs
+++ Unit_1-Player Control/Prototype 1/Assets/Scripts/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     public float speed = 20.0f;
     public float turnSpeed;
+    public float horizontalInput;
 
     // Start is called before the first frame update
     void Start()
@@ -16,8 +17,11 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Get the user's movements
+        horizontalInput = Input.GetAxis("Horizontal");
+
         // Move the vehicle forward
         transform.Translate(Vector3.forward * Time.deltaTime * speed);
-        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed);
+        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
