cb8578041316f2bbda89ade1e80a98318dc81672
Vincent Sposato <vincent.sposato@gmail.com>
2024-09-01 11:46:00 -0400

    Lesson 2.4.1 - make a spawn animal method

--- Unit_2-Basic Gameplay/Prototype 2/Assets/Scripts/SpawnManager.cs
+++ Unit_2-Basic Gameplay/Prototype 2/Assets/Scripts/SpawnManager.cs
@@ -18,10 +18,15 @@ public class SpawnManager : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.S))
         {
-            // Randomly generate animal index and spawn position
-            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
-            int animalIndex = Random.Range(0, animalPrefabs.Length);
-            Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
+            SpawnRandomAnimal();
         }
+    }
+
+    void SpawnRandomAnimal()
+    {
+        // Randomly generate animal index and spawn position
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
     }
 }
