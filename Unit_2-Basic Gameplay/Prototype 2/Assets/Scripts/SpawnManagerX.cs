58d94216cbb17c1955123cbf7af3ca9ba431668b
Vincent Sposato <vincent.sposato@gmail.com>
2024-09-01 12:47:34 -0400

    Challenge 2.7 - fixed random ball selection

--- Unit_2-Basic Gameplay/Prototype 2/Assets/Scripts/SpawnManagerX.cs
+++ Unit_2-Basic Gameplay/Prototype 2/Assets/Scripts/SpawnManagerX.cs
@@ -23,10 +23,11 @@ public class SpawnManagerX : MonoBehaviour
     void SpawnRandomBall ()
     {
         // Generate random ball index and random spawn position
+        int ballIndex = Random.Range(0, ballPrefabs.Length);
         Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
 
         // instantiate ball at random spawn location
-        Instantiate(ballPrefabs[0], spawnPos, ballPrefabs[0].transform.rotation);
+        Instantiate(ballPrefabs[ballIndex], spawnPos, ballPrefabs[ballIndex].transform.rotation);
     }
 
 }
