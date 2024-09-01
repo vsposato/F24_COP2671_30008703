b033089bc8b01ac4ded90a36cc8add05e3e00e60
Vincent Sposato <vincent.sposato@gmail.com>
2024-09-01 10:58:24 -0400

    Lesson 2.2.3 - added space bar capture in player controller

--- Unit_2-Basic Gameplay/Prototype 2/Assets/Scripts/PlayerController.cs
+++ Unit_2-Basic Gameplay/Prototype 2/Assets/Scripts/PlayerController.cs
@@ -18,6 +18,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         if (transform.position.x < -xRange)
         {
             transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
@@ -26,7 +27,13 @@ public class PlayerController : MonoBehaviour
         {
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
+
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Launch a projectile from the player
+        }
     }
 }
