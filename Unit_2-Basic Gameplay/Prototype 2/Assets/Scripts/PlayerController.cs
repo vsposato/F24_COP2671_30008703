5860283f2377f73a48dd9283af7db5cc28282f0b
Vincent Sposato <vincent.sposato@gmail.com>
2024-09-01 10:55:51 -0400

    Lesson 2.2.2 - prefab projectile created and added to player object

--- Unit_2-Basic Gameplay/Prototype 2/Assets/Scripts/PlayerController.cs
+++ Unit_2-Basic Gameplay/Prototype 2/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,8 @@ public class PlayerController : MonoBehaviour
     public float horizontalInput;
     public float speed = 10.0f;
     public float xRange = 15.0f;
+    public GameObject projectilePrefab;
+
     // Start is called before the first frame update
     void Start()
     {
