f6a9be9ec2cae79af81917203dd7e73dc2e353c3
Vincent Sposato <vincent.sposato@gmail.com>
2024-09-01 11:05:55 -0400

    Lesson 2.2.4 - created prefabs from animals and added move forward script

--- Unit_2-Basic Gameplay/Prototype 2/Assets/Scripts/PlayerController.cs
+++ Unit_2-Basic Gameplay/Prototype 2/Assets/Scripts/PlayerController.cs
@@ -34,6 +34,7 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             // Launch a projectile from the player
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
         }
     }
 }
