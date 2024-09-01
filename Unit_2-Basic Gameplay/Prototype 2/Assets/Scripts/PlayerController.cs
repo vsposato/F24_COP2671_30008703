b21c796d907dad3028bd55c8292bcf627fe707da
Vincent Sposato <vincent.sposato@gmail.com>
2024-09-01 10:41:49 -0400

    Lesson 2.1.6 - added full x bounds to player control

--- Unit_2-Basic Gameplay/Prototype 2/Assets/Scripts/PlayerController.cs
+++ Unit_2-Basic Gameplay/Prototype 2/Assets/Scripts/PlayerController.cs
@@ -6,7 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
     public float speed = 10.0f;
-
+    public float xRange = 15.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,9 +16,13 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.x < -10)
+        if (transform.position.x < -xRange)
         {
-            transform.position = new Vector3(-10, transform.position.y, transform.position.z);
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
