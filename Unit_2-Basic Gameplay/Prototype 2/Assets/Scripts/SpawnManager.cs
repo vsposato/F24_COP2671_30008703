4b830b218d0ac6a951799f7d69a7ed5b0a4cbf06
Vincent Sposato <vincent.sposato@gmail.com>
2024-09-01 11:54:36 -0400

    Lesson 2.4.2 - added automatic spawning of animals to the spawn manager

--- Unit_2-Basic Gameplay/Prototype 2/Assets/Scripts/SpawnManager.cs
+++ Unit_2-Basic Gameplay/Prototype 2/Assets/Scripts/SpawnManager.cs
@@ -7,19 +7,19 @@ public class SpawnManager : MonoBehaviour
     public GameObject[] animalPrefabs;
     private float spawnRangeX = 20.0f;
     private float spawnPosZ = 20.0f;
+    private float startDelay = 2.0f;
+    private float spawnInterval = 1.5f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            SpawnRandomAnimal();
-        }
+
     }
 
     void SpawnRandomAnimal()
