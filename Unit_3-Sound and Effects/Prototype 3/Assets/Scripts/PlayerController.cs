2063a9f0be386820644d95edfbf55fbf302347a8
Vincent Sposato <vincent.sposato@gmail.com>
2024-09-02 09:41:09 -0400

    Lesson 3.1.3 - added jump at start to player

--- Unit_3-Sound and Effects/Prototype 3/Assets/Scripts/PlayerController.cs
+++ Unit_3-Sound and Effects/Prototype 3/Assets/Scripts/PlayerController.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private Rigidbody playerRb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody>();
+        playerRb.AddForce(Vector3.up * 1000);
     }
 
     // Update is called once per frame
