af71553a666ff4013d94ecdf7947ad7a28fbe3dc
Vincent Sposato <vincent.sposato@gmail.com>
2024-09-02 10:53:56 -0400

    Lesson 3.2.7 destroy obstacles that leave the bounds of the game

--- Unit_3-Sound and Effects/Prototype 3/Assets/Scripts/MoveLeft.cs
+++ Unit_3-Sound and Effects/Prototype 3/Assets/Scripts/MoveLeft.cs
@@ -6,6 +6,7 @@ public class MoveLeft : MonoBehaviour
 {
     private float speed = 30.0f;
     private PlayerController playerControllerScript;
+    private float leftBound = -15.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +19,11 @@ public class MoveLeft : MonoBehaviour
         if (playerControllerScript.gameOver == false)
         {
             transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
+
+        if (transform.position.x < leftBound && gameObject.CompareTag("Obstacle"))
+        {
+            Destroy(gameObject);
         }
     }
 }
