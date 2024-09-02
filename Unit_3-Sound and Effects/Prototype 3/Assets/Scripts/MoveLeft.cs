4f877da5426635bceccd86d08e9d4fec670c9767
Vincent Sposato <vincent.sposato@gmail.com>
2024-09-02 10:47:10 -0400

    Lesson 3.2.5 stop the move left if the game is over

--- Unit_3-Sound and Effects/Prototype 3/Assets/Scripts/MoveLeft.cs
+++ Unit_3-Sound and Effects/Prototype 3/Assets/Scripts/MoveLeft.cs
@@ -5,15 +5,19 @@ using UnityEngine;
 public class MoveLeft : MonoBehaviour
 {
     private float speed = 30.0f;
+    private PlayerController playerControllerScript;
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.left * Time.deltaTime * speed);
+        if (playerControllerScript.gameOver == false)
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
     }
 }
