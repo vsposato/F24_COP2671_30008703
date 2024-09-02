21b363fa38f54d2e96bac0f8045300a282938f4d
Vincent Sposato <vincent.sposato@gmail.com>
2024-09-02 10:43:09 -0400

    Lesson 3.2.4 added game over trigger and updated collision logic

--- Unit_3-Sound and Effects/Prototype 3/Assets/Scripts/PlayerController.cs
+++ Unit_3-Sound and Effects/Prototype 3/Assets/Scripts/PlayerController.cs
@@ -8,6 +8,7 @@ public class PlayerController : MonoBehaviour
     public float jumpForce = 10.0f;
     public float gravityModifier;
     public bool isOnGround = true;
+    public bool gameOver = false;
 
     // Start is called before the first frame update
     void Start()
@@ -28,6 +29,13 @@ public class PlayerController : MonoBehaviour
 
     void OnCollisionEnter(Collision collision)
     {
-        isOnGround = true;    
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        } else if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            gameOver = true;
+            Debug.Log("Game Over!");
+        }
     }
 }
