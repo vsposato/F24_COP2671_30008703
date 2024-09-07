f318875350c6475371239c3708b52807a6a30b5c
Vincent Sposato <vincent.sposato@gmail.com>
2024-09-07 12:43:18 -0400

    Lesson 4.4.5 - Spawn Powerups with new waves

--- Unit_4-Gameplay Mechanics/Prototype 4/Assets/Scripts/SpawnManager.cs
+++ Unit_4-Gameplay Mechanics/Prototype 4/Assets/Scripts/SpawnManager.cs
@@ -5,13 +5,16 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject enemyPrefab;
+    public GameObject powerupPrefab;
+
     private float spawnRange = 9.0f;
-    public int enemyCount;
+    public int enemyCount = 1;
     public int waveNumber = 1;
     // Start is called before the first frame update
     void Start()
     {
         SpawnEnemyWave(waveNumber);
+        Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation);
     }
 
     // Update is called once per frame
@@ -22,6 +25,7 @@ public class SpawnManager : MonoBehaviour
         {
             waveNumber++;
             SpawnEnemyWave(waveNumber);
+            Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation);
         }
     }
 
