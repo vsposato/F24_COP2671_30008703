3db6a7599b6713de6ef4955607ea502cc1d8b6e3
Vincent Sposato <vincent.sposato@gmail.com>
2024-09-07 11:59:46 -0400

    Lesson 4.3.6 - Add a powerup indicator

--- Unit_4-Gameplay Mechanics/Prototype 4/Assets/Scripts/PlayerController.cs
+++ Unit_4-Gameplay Mechanics/Prototype 4/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
     private Rigidbody playerRb;
     private GameObject focalPoint;
     private float powerupStrength = 15.0f;
+    public GameObject powerupIndicator;
     public float speed = 5.0f;
     public bool hasPowerup = false;
 
@@ -22,6 +23,7 @@ public class PlayerController : MonoBehaviour
     {
         float forwardInput = Input.GetAxis("Vertical");
         playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
+        powerupIndicator.transform.position = transform.position + new Vector3(0, -0.5f, 0);
     }
 
     void OnTriggerEnter(Collider other)
@@ -29,6 +31,7 @@ public class PlayerController : MonoBehaviour
         if (other.CompareTag("Powerup"))
         {
             hasPowerup = true;
+            powerupIndicator.gameObject.SetActive(true);
             Destroy(other.gameObject);
             StartCoroutine(PowerupCountdownRoutine());
         }
@@ -38,6 +41,7 @@ public class PlayerController : MonoBehaviour
     {
         yield return new WaitForSeconds(7);
         hasPowerup = false;
+        powerupIndicator.gameObject.SetActive(false);
     }
 
     void OnCollisionEnter(Collision collision)
