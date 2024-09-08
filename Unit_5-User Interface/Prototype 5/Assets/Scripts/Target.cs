c6c6b02b9fddd45c9eef0e5c1f4c26269191f3f2
Vincent Sposato <vincent.sposato@gmail.com>
2024-09-08 09:19:55 -0400

    Lesson 5.1.4 - Replace messy code with new methods

--- Unit_5-User Interface/Prototype 5/Assets/Scripts/Target.cs
+++ Unit_5-User Interface/Prototype 5/Assets/Scripts/Target.cs
@@ -4,14 +4,38 @@ public class Target : MonoBehaviour
 {
     private Rigidbody targetRb;
 
+    private float minSpeed = 12.0f;
+
+    private float maxSpeed = 16.0f;
+
+    private float maxTorque = 10.0f;
+
+    private float xRange = 4.0f;
+
+    private float ySpawnPos = -6.0f;
+
     // Start is called before the first frame update
     void Start()
     {
         targetRb = GetComponent<Rigidbody>();
-        targetRb.AddForce(Vector3.up * Random.Range(12, 16), ForceMode.Impulse);
-        targetRb.AddTorque(Random.Range(-10, 10), Random.Range(-10, 10), Random.Range(-10, 10),
-            ForceMode.Impulse);
-        transform.position = new Vector3(Random.Range(-4, 4), -6);
+        targetRb.AddForce(RandomForce(), ForceMode.Impulse);
+        targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
+        transform.position = RandomSpawnPos();
+    }
+
+    Vector3 RandomForce()
+    {
+        return Vector3.up * Random.Range(minSpeed, maxSpeed);
+    }
+
+    float RandomTorque()
+    {
+        return Random.Range(-maxTorque, maxTorque);
+    }
+
+    Vector3 RandomSpawnPos()
+    {
+        return new Vector3(Random.Range(-xRange, xRange), ySpawnPos);
     }
 
     // Update is called once per frame
