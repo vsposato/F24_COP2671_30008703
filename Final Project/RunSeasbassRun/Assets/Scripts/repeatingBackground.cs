677fae59b313034b130d32c69e51870dcd45d5e3
Vincent Sposato <vincent.sposato@gmail.com>
2024-10-06 12:46:10 -0400

    Setting up first level scene with player and camera adjustments

--- Final Project/RunSeasbassRun/Assets/Scripts/repeatingBackground.cs
+++ Final Project/RunSeasbassRun/Assets/Scripts/repeatingBackground.cs
@@ -12,17 +12,17 @@ public class repeatingBackground : MonoBehaviour
 	{
 		//Get and store a reference to the collider2D attached to Ground.
 		groundCollider = GetComponent<BoxCollider2D> ();
-		//Store the size of the collider along the x axis (its length in units).
+		//Store the size of the collider along the x-axis (its length in units).
 		groundHorizontalLength = groundCollider.size.x;
 	}
 
 	//Update runs once per frame
 	private void Update()
 	{
-		//Check if the difference along the x axis between the main Camera and the position of the object this is attached to is greater than groundHorizontalLength.
+		//Check if the difference along the x-axis between the main Camera and the position of the object this is attached to is greater than groundHorizontalLength.
 		if (transform.position.x < -groundHorizontalLength)
 		{
-			//If true, this means this object is no longer visible and we can safely move it forward to be re-used.
+			//If true, this means this object is no longer visible, and we can safely move it forward to be re-used.
 			RepositionBackground ();
 		}
 	}
@@ -33,7 +33,7 @@ public class repeatingBackground : MonoBehaviour
 		//This is how far to the right we will move our background object, in this case, twice its length. This will position it directly to the right of the currently visible background object.
 		Vector2 groundOffSet = new Vector2(groundHorizontalLength * 2f, 0);
 
-		//Move this object from it's position offscreen, behind the player, to the new position off-camera in front of the player.
+		//Move this object from its position offscreen, behind the player, to the new position off-camera in front of the player.
 		transform.position = (Vector2) transform.position + groundOffSet;
 	}
 }
