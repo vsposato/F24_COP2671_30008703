c47298e25e13b8b19143d6b3b446bb53df85019d
Vincent Sposato <vincent.sposato@gmail.com>
2024-10-06 10:48:37 -0400

    Final project - importing resources and getting base project setup

---
+++ Final Project/RunSeasbassRun/Assets/Resources/CartoonTilingBackgrounds/scripts/ScrollingObject.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScrollingObject : MonoBehaviour 
+{
+	// Use this for initialization
+	public float scrollSpeed;
+
+	//Update runs once per frame
+	private void Update(){
+		transform.position += new Vector3(scrollSpeed,0,0) * Time.deltaTime;
+	}
+
+}
